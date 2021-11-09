1f80bf03d179d6761e2a2b14cfe4a6ef2d82125c
DESKTOP-S2Q9BCV\Shinta <jojoshinarneta2000@gmail.com>
2021-11-09 11:51:31 +0800

    Tahap 6 mid

---
+++ resources/views/SoalMidKakasNo5.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>1.	Version control adalah sebuah sistem yang mencatat setiap perubahan terhadap sebuah berkas atau kumpulan berkas sehingga pada suatu saat anda dapat kembali kepada salah satu versi dari berkas tersebut. Sistem ini memungkinkan anda untuk mengembalikan berkas anda pada kondisi/keadaan sebelumnya, mengembalikan seluruh proyek pada keadaan sebelumnya, membandingkan perubahan setiap saat, melihat siapa yang terakhir melakukan perubahan terbaru pada suatu objek sehingga berpotensi menimbulkan masalah, siapa yang menerbitkan isu, dan lainnya.</p>
+    <h3>Macam-macam VCS</h2>
+    <h3>- GIT</h3>
+    <h3>- CVS</h3>
+    <h3>- SVN</h3>
+    <h3>- Mercurial</h3>
+    <h3>- Monotone</h3>
+    <h3>- Bazaar</h3>
+    <h3>- TFS</h3>
+    <h3>- VSTS</h3>
+    <h3>- Perforce Helix Core</h3>
+    <h3>- IBM Rational ClearCase</h3>
+</body>
+</html>
